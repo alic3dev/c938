49bbe0115d5b70377aa5b5a84c79985a6d38120f
alic3 <alic3dev@gmail.com>
2025-09-17 16:51:03 -0700

    [`player_data`|`player`]:add_boosts

--- include/player_data.h
+++ include/player_data.h
@@ -9,10 +9,12 @@ struct player_data {
   struct metil_object** objects;
   unsigned short int length_objects;
 
+  unsigned long int time_boost;
   unsigned long int time_jump;
 
-  float is_jumping;
-  float is_jumping_secondary;
+  unsigned char is_boosted;
+  unsigned char is_jumping;
+  unsigned char is_jumping_secondary;
 
   unsigned int has_collided;
   unsigned int on_ground;
