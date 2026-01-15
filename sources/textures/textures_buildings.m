afaad1d82a39725d8b0a846a033e1cedb3dd7db3
alic3 <alic3dev@gmail.com>
2026-01-14 18:30:19 -0800

    `textures_building`:use->{`metil_texture_brightness`};

--- sources/textures/textures_buildings.m
+++ sources/textures/textures_buildings.m
@@ -1,5 +1,7 @@
 #include <textures/textures_buildings.h>
 
+#include <metil_texture/metil_texture_brightness.h>
+#include <metil_image/metil_image_type.h>
 #include <metil_texture/metil_texture_concrete.h>
 
 #include <Metal/MTLTexture.h>
@@ -51,78 +53,11 @@ void textures_buildings_load(
       metil->renderer_interface.metal_device
     );
 
-    MTLRegion region = {
-      {0x00, 0x00, 0x00},
-      {0xc8, 0xc8, 0x01}
-    };
-
-    unsigned char* pixel_bytes = (
-      (void*) 0
-    );
-
-    pixel_bytes = (
-      malloc(
-        sizeof(
-          unsigned char
-        ) *
-        0x27100
-      )
-    );
-
-    [
+    metil_texture_brightness(
       textures[
         0
-      ]
-      getBytes: pixel_bytes
-      bytesPerRow: 0x320
-      fromRegion: region
-      mipmapLevel: 0x00
-    ];
-
-    for (
-      unsigned int index_pixel = 0x00;
-      index_pixel < 0x27100;
-      ++index_pixel
-    ) {
-      if (
-        (
-          index_pixel +
-          1
-        ) %
-        4 ==
-        0
-      ) {
-        continue;
-      }
-
-      if (
-        pixel_bytes[
-          index_pixel
-        ] > 0x80
-      ) {
-        pixel_bytes[
-          index_pixel
-        ] = (
-          pixel_bytes[
-            index_pixel
-          ] /
-          0b0100
-        );
-      }
-    }
-
-    [
-      textures[
-        0
-      ]
-      replaceRegion: region
-      mipmapLevel: 0x00
-      withBytes: pixel_bytes
-      bytesPerRow: 0x320
-    ];
-
-    free(
-      pixel_bytes
+      ],
+      0.05f
     );
   }
 }
