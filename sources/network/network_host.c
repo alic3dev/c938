1773bbb2e2d58623e94418106824b6879b1f967a
alic3 <alic3dev@gmail.com>
2026-01-17 00:52:05 -0800

    `network_host`:add

---
+++ sources/network/network_host.c
@@ -0,0 +1,276 @@
+#include <network/network_host.h>
+
+#include <clic3_char_arrays.h>
+#include <clic3_memory.h>
+
+#include <errno.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+unsigned char network_host_listen(
+  struct network_host* network_host,
+  unsigned int length_threads
+) {
+  network_host->socket = (
+    socket(
+      AF_INET,
+      SOCK_STREAM,
+      0
+    )
+  );
+
+  if (
+    network_host->socket < 0
+  ) {
+    return 1;
+  }
+
+  network_host->address_socket.sin_family = (
+    AF_INET
+  );
+  
+  network_host->address_socket.sin_port = (
+    htons(
+      c938_network_host_port
+    )
+  );
+
+  network_host->address_socket.sin_addr.s_addr = (
+    htonl(
+      INADDR_ANY
+    )
+  );
+
+  int status_bind = (
+    bind(
+      network_host->socket,
+      (
+        (struct sockaddr*)
+        &network_host->address_socket
+      ),
+      sizeof(
+        network_host->address_socket
+      )
+    )
+  );
+
+  if (
+    status_bind < 0
+  ) {
+    return 2;
+  }
+
+  int status_listen = (
+    listen(
+      network_host->socket,
+      1
+    )
+  );
+
+  if (
+    status_listen < 0
+  ) {
+    return 3;
+  }
+
+  FD_ZERO(
+    &network_host->file_descriptor_socket_set
+  );
+
+  FD_SET(
+    network_host->socket,
+    &network_host->file_descriptor_socket_set
+  );
+
+  network_host->online = 1;
+
+  network_host->length_threads = (
+    length_threads
+  );
+
+  network_host->threads = (
+    clic3_memory_allocate_raw(
+      sizeof(
+        pthread_t
+      )
+    )
+  );
+
+  for (
+    unsigned int index_thread = 0;
+    index_thread < network_host->length_threads;
+    ++index_thread
+  ) {
+    pthread_create(
+      &network_host->threads[
+        index_thread
+      ],
+      0,
+      network_host_thread,
+      network_host
+    );
+  }
+
+  network_host->length_notification_on = 0;
+
+  network_host->notification_on = (
+    clic3_memory_allocate_raw(
+      0
+    )
+  );
+
+  return 0;
+}
+
+void* network_host_thread(
+  void* data
+) {
+  struct network_host* network_host = (
+    data
+  );
+
+  while (
+    network_host->online == 1
+  ) {
+    int status_select = (
+      select(
+        FD_SETSIZE,
+        &network_host->file_descriptor_socket_set,
+        0,
+        0,
+        0
+      )
+    );
+
+    if (
+      status_select != 1
+    ) {
+      continue;
+    }
+
+    struct sockaddr address_socket_client;
+    socklen_t length_socket_client;
+
+    int socket_client = (
+      accept(
+        network_host->socket,
+        &address_socket_client,
+        &length_socket_client
+      )
+    );
+
+    
+    char* char_array_address_client = (
+      clic3_memory_allocate_raw(
+        address_socket_client.sa_len +
+        1
+      )
+    );
+
+    for (
+      unsigned char index_char_array_address_client = 0;
+      index_char_array_address_client < address_socket_client.sa_len;
+      ++index_char_array_address_client
+    ) {
+      char_array_address_client[
+        index_char_array_address_client
+      ] = (
+        address_socket_client.sa_data[
+          index_char_array_address_client
+        ]
+      );
+    }
+
+    char_array_address_client[
+      address_socket_client.sa_len
+    ] = '\0';
+
+    if (
+      socket_client < 0
+    ) {
+      continue;
+    }
+
+    char* notification_prefix = (
+      clic3_char_arrays_concatenate(
+        "client_connected->{",
+        char_array_address_client
+      )
+    );
+
+    clic3_memory_free_raw(
+      char_array_address_client
+    );
+
+    char* notification = (
+      clic3_char_arrays_concatenate(
+        notification_prefix,
+        "};"
+      )
+    );
+
+    clic3_memory_free_raw(
+      notification_prefix
+    );
+
+    network_host_notification_send(
+      network_host,
+      notification
+    );
+
+    clic3_memory_free_raw(
+      notification
+    );
+
+    close(
+      socket_client
+    );
+  }
+
+  return 0;
+}
+
+void network_host_notification_send(
+  struct network_host* network_host,
+  char* notification
+) {
+  for (
+    unsigned char index_notification_on = 0;
+    index_notification_on < network_host->length_notification_on;
+    ++index_notification_on
+  ) {
+    network_host->notification_on[
+      index_notification_on
+    ](
+      notification
+    );
+  }
+}
+
+void network_host_notification_on_add(
+  struct network_host* network_host,
+  network_host_notification_on notification_on
+) {
+  network_host->length_notification_on = (
+    network_host->length_notification_on +
+    1
+  );
+
+  network_host->notification_on = (
+    clic3_memory_allocate_raw(
+      sizeof(
+        network_host_notification_on
+      ) *
+      network_host->length_notification_on
+    )
+  );
+
+  network_host->notification_on[
+    network_host->length_notification_on -
+    1
+  ] = (
+    notification_on
+  );
+}
