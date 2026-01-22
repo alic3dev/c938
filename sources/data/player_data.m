a2c50cb6d596628638414b7dcc43a9c909c691d1
alic3 <alic3dev@gmail.com>
2026-01-21 18:00:45 -0800

    `player_data.m`->`data/player_data.m`

--- sources/data/player_data.m
+++ sources/data/player_data.m
@@ -4,33 +4,26 @@ void player_data_initialize(
   struct player_data* player_data,
   float height
 ) {
-  player_data->buildings = (
-    (void*) 0
-  );
+  player_data->buildings = 0;
+  player_data->projectiles = 0;
 
-  player_data->projectiles = (
-    (void*) 0
-  );
-
-  player_data->time_jump = 0;
-  player_data->time_boost = 0;
-  player_data->time_shot = 0;
+  player_data->height = height;
 
   player_data->is_jumping = 0;
   player_data->is_jumping_secondary = 0;
 
   player_data->is_boosted = 0;
 
-  player_data->shooting = 0;
-
-  player_data->height = height;
-
   player_data->life_maximum = 3;
   player_data->life = player_data->life_maximum;
 
   player_data->rate_fire = player_data_default_rate_fire;
 
-  player_data->time = (
-    (void*) 0
-  );
+  player_data->shooting = 0;
+
+  player_data->time = 0;
+
+  player_data->time_jump = 0;
+  player_data->time_boost = 0;
+  player_data->time_shot = 0;
 }
