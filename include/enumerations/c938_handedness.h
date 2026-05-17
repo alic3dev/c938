1bd5701a453a040875cf21306d0e7e2fcf3cddc8
alic3 <alic3dev@gmail.com>
2026-05-17 14:47:50 -0700

    `c938_handedness`:add

---
+++ include/enumerations/c938_handedness.h
@@ -0,0 +1,10 @@
+#ifndef __c938_enumerations_c938_handedness_h
+#define __c938_enumerations_c938_handedness_h
+
+enum c938_handedness {
+  c938_handedness_none  = 0x00,
+  c938_handedness_left  = 0x01,
+  c938_handedness_right = 0x02
+};
+
+#endif
