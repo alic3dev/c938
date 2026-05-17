aeda812e64042ee7f35723d1705ab8f3a5c5d68b
alic3 <alic3dev@gmail.com>
2026-05-17 14:48:06 -0700

    `mesh_gun`:add

---
+++ include/mesh/mesh_gun.h
@@ -0,0 +1,11 @@
+#ifndef __c938_mesh_mesh_gun_h
+#define __c938_mesh_mesh_gun_h
+
+#include <metil_mesh/metil_mesh.h>
+
+void mesh_gun_initialize(
+  struct metil_mesh*,
+  unsigned char
+);
+
+#endif
---
+++ sources/mesh/mesh_gun.c
@@ -0,0 +1,100 @@
+#include <mesh/mesh_gun.h>
+
+#include <enumerations/c938_handedness.h>
+
+void mesh_gun_initialize(
+  struct metil_mesh* c938_mesh_gun,
+  unsigned char handedness
+) {
+  float inverter_x;
+
+  if (
+    handedness !=
+    c938_handedness_left
+  ) {
+    inverter_x = -(
+      0x01
+    );
+  } else {
+    inverter_x = (
+      0x01
+    );
+  }
+
+  metil_mesh_initialize_with_lengths(
+    c938_mesh_gun,
+    0x0f,
+    (
+      0x0f *
+      0x02
+    )
+  );
+  
+  for (
+    unsigned int index_vertex = (
+      0x00
+    );
+    (
+      index_vertex <
+      c938_mesh_gun->length_vertices
+    );
+    ++index_vertex
+  ) {
+    c938_mesh_gun->vertices[
+      index_vertex
+    ].x = (
+      c938_mesh_gun->vertices[
+        index_vertex
+      ].x *
+      inverter_x
+    );
+    
+    c938_mesh_gun->vertices[
+      index_vertex
+    ].w = (
+      0x01
+    );
+  }
+}
+
+/*
+
+  _|______:__.
+ |_^__,:__ _|*/
+ |_||(_/ | |\
+|:_|/
+  
+  
+          
+       *              .  /
+           ^    /  .  .
+           \.\ / /
+            _____
+ _           /.|. /|\
+
+         / _  / /_|
+        / _  ///|
+_\       /    / /  \./
+    _|__|_ //|
+   |  []  |/(|
+    |    |/|/
+    |___ |/|
+   /|____|\/
+
+
+
+
+      .__|:|_.
+      |||(*)||       |\__/|
+        \___/
+        
+        gun
+*/
+
+/*
+
+    .
+   . . .
+  .
+*/
+
