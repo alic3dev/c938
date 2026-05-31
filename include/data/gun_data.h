4cddddfbc93c440d28622d34eb03e96ce3fc90df
alic3dev <alice@alic3.dev>
2026-05-30 21:56:28 -0700

    `gun_data`:add

---
+++ include/data/gun_data.h
@@ -0,0 +1,12 @@
+#ifndef __c938_data_gun_data_h
+#define __c938_data_gun_data_h
+
+#include <metil_animation/metil_animation.h>
+
+struct c938_data_gun_data {
+  struct metil_animation animation_idle;
+  struct metil_animation animation_firing;
+};
+
+#endif
+
